802ad851252fe096cd5e3b577b271afb8798acff
Die, Master Monkey <lofimonkey@gmail.com>
2016-03-16 02:42:07 -0700

    Create example-run-windows-mfz.bat
    Simple tip for starting on Windows, with mfz-rxtx (as opposed to regular JRE rxtx)

---
+++ example-run-windows-mfz.bat
@@ -0,0 +1,6 @@
+REM Example run script for Windoze
+REM Assuming your mfz-rxtx is installed in (relative path) mfz-rxtx
+REM and everything else is in your current directory.
+REM @diemastermonkey 2:41 AM 3/16/2016
+REM 
+java -cp mfz-rxtx\;. MBTCPClient 
